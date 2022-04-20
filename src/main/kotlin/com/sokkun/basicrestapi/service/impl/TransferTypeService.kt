0e823854a3c784677a5e7be85e3810881af61b97
sokkun <sokkun.norn2019@gmail.com>
2022-04-20 09:35:35 +0700

    init func create transfer type

--- src/main/kotlin/com/sokkun/basicrestapi/service/impl/TransferTypeService.kt
+++ src/main/kotlin/com/sokkun/basicrestapi/service/impl/TransferTypeService.kt
@@ -1,6 +1,8 @@
 package com.sokkun.basicrestapi.service.impl
 
+import com.sokkun.basicrestapi.api.request.TransferTypeReq
 import com.sokkun.basicrestapi.api.response.TransferTypeRes
+import com.sokkun.basicrestapi.model.TransferType
 import com.sokkun.basicrestapi.repository.TransferTypeRepository
 import com.sokkun.basicrestapi.service.ITransferTypeService
 import org.springframework.stereotype.Service
@@ -10,4 +12,10 @@ class TransferTypeService(
     private val transferTypeRepo: TransferTypeRepository
 ) : ITransferTypeService {
     override fun findAllTransferType(): List<TransferTypeRes> = transferTypeRepo.findAll().map { TransferTypeRes(it.id, it.name) }
+    override fun createTransferType(transferTypeReq: TransferTypeReq): TransferTypeRes {
+        val transferType = TransferType(0, transferTypeReq.name, transferTypeReq.description)
+        val createTransferType = transferTypeRepo.save(transferType)
+
+        return TransferTypeRes(createTransferType.id, createTransferType.name)
+    }
 }
